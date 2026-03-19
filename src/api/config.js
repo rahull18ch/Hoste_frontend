bec0bbd1222aeb762f2acfbb50b611896e3b7a80
Rahul <rahull727788@gmail.com>
2026-03-19 08:31:38 +0530

    chatbot

--- src/api/config.js
+++ src/api/config.js
@@ -1,10 +1,10 @@
 // src/api/config.js
-// const BASE_URL = "https://hoste-backend.vercel.app";//
+const BASE_URL = "https://hoste-backend.vercel.app";
 //http://localhost:4000/route
 // http://localhost:4000/
+// https://hoste-backend.vercel.app/
 
-
-const BASE_URL = "http://localhost:4000";
+// const BASE_URL = "http://localhost:4000";
 
 
 export default BASE_URL;
